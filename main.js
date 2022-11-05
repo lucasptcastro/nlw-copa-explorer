c4fc5c339c42c40d47edd8fd7bf4a70dad468937
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2022-11-05 10:02:29 -0300

    ✨ feat: adicao do html no js

---
+++ main.js
@@ -0,0 +1,41 @@
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="assets/icon-${player1}.svg" alt="Bandeira da ${player1}" />
+          <strong>${hour}</strong>
+      <img src="assets/icon-${player2}.svg" alt="Bandeira de ${player2}" />
+    </li>
+    `;
+}
+
+function createCard(date, day, games) {
+  return `
+    <div class="card">
+    <h2>${date} <span>${day}</span></h2>
+    <ul>
+    <!-- Tag de lista não ordenada-->
+    ${games}
+    </ul>
+    </div>
+    `;
+}
+
+document.querySelector("#app").innerHTML = `
+    <header><img src="./assets/logo.svg" alt="Logo da NLW" /></header>
+    <main id="cards">
+    ${createCard(
+      "21/11",
+      "segunda",
+      createGame("brazil", "08:00", "cameroon") +
+        createGame("hungary", "13:00", "argentina") +
+        createGame("colombia", "20:00", "japan")
+    )}
+    ${createCard(
+      "24/11",
+      "quinta",
+      createGame("british-columbia", "08:00", "cameroon") +
+        createGame("india", "13:00", "armenia") +
+        createGame("comoros", "20:00", "jordan")
+    )}
+    </main>
+`;
