c24438aa6b454a69bb4ff4a163277fe7b7606217
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2022-11-07 00:26:40 -0300

    🦄 refactor: refatoracao

--- main.js
+++ main.js
@@ -20,22 +20,18 @@ function createCard(date, day, games) {
     `;
 }
 
-document.querySelector("#app").innerHTML = `
-    <header><img src="./assets/logo.svg" alt="Logo da NLW" /></header>
-    <main id="cards">
-    ${createCard(
-      "21/11",
-      "segunda",
-      createGame("brazil", "08:00", "cameroon") +
-        createGame("hungary", "13:00", "argentina") +
-        createGame("colombia", "20:00", "japan")
-    )}
-    ${createCard(
-      "24/11",
-      "quinta",
-      createGame("british-columbia", "08:00", "cameroon") +
-        createGame("india", "13:00", "armenia") +
-        createGame("comoros", "20:00", "jordan")
-    )}
-    </main>
-`;
+document.querySelector("#app").innerHTML =
+  createCard(
+    "21/11",
+    "segunda",
+    createGame("brazil", "08:00", "cameroon") +
+      createGame("hungary", "13:00", "argentina") +
+      createGame("colombia", "20:00", "japan")
+  ) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("british-columbia", "08:00", "cameroon") +
+      createGame("india", "13:00", "armenia") +
+      createGame("comoros", "20:00", "jordan")
+  );
